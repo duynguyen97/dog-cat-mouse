1ab414d0d5659ca2bba35bf5a1135465985ebf84
duynb <duynb@runsystem.net>
2019-09-27 17:11:46 +0700

    Horse sleep

--- Horse.js
+++ Horse.js
@@ -1,7 +1,13 @@
 function Horse(){
 
 }
+
 Horse.prototype.run = function(){
 	console.log("Running...");
 }
+
+Horse.prototype.sleep = function(){
+	console.log("Sleeping...");
+}
+
 module.exports = Horse;
